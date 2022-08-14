9f4621f16279476663b70ae519966731d55876ae
Raghav Sharma <raghav_1205@outlook.com>
2022-08-15 03:12:31 +0530

    bug fixes

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,48 +1,56 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router';
 import { UserContext } from '../lib/context'
 import { useContext } from 'react'
 
-const Navbar = () => {
-  const  {user, username } = useContext(UserContext)
-
-  return (
-    <nav className='navbar'>
+// Top navbar
+export default function Navbar() {
+    const { user, username } = useContext(UserContext);
+  
+    const router = useRouter();
+  
+    const signOut =  () => {
+      auth.signOut();
+      router.reload();
+    }
+  
+    return (
+      <nav className="navbar">
         <ul>
-        <li>
-            <Link href = '/'>
-                <button className='btn-logo'>FEED</button>
+          <li>
+            <Link href="/">
+              <button className="btn-logo">NXT</button>
             </Link>
-        </li>
-
-        {username && (
+          </li>
+  
+          {/* user is signed-in and has username */}
+          {username && (
             <>
-
-            <li className='push-left'>
-                <Link href='/admin'>
-                    <button className='btn-blue'>Write Posts</button>
+              <li className="push-left">
+                <button onClick={signOut}>Sign Out</button>
+              </li>
+              <li>
+                <Link href="/admin">
+                  <button className="btn-blue">Write Posts</button>
                 </Link>
-            </li>
-            <li>
-                <Link href = {`/${username}`}>
-                    <img src = {user?.photoURL}/>
+              </li>
+              <li>
+                <Link href={`/${username}`}>
+                  <img src={user?.photoURL || '/hacker.png'} />
                 </Link>
-            </li>
-            
+              </li>
             </>
-        )}
-
-        {!username && (
-            <>
+          )}
+  
+          {/* user is not signed OR has not created username */}
+          {!username && (
             <li>
-                <Link href= '/enter'>
-                    <button className='btn-blue'>Login</button>    
-                </Link>
+              <Link href="/enter">
+                <button className="btn-blue">Log in</button>
+              </Link>
             </li>
-            </>
-        )}
+          )}
         </ul>
-    </nav>
-  )
-}
-
-export default Navbar
+      </nav>
+    );
+  }
